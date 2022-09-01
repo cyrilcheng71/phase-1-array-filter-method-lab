4011b88f229da576db2b7e68c9ef8e31d71f8111
Man Cheng <cyrilcheng@protonmail.com>
2022-08-31 20:37:58 -0700

    commit tree 8a111ed80ceb3806933bcb2c93de97d138f272ed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+
+
+function findMatching(arr, string){
+    return arr.filter(driver => string.toLowerCase() === driver.toLowerCase())
+}
+
+
+function fuzzyMatch(arr, string){
+    return arr.filter(driver => driver.startsWith(string))
+}
+
+function matchName(arr, string){
+    return arr.filter(driver => driver.name === string)
+}
